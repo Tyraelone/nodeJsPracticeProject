1f81659997f4ef87122e1fed88b31080aa7ce772
Administrator <duanziyu3@163.com>
2015-11-23 14:51:15 +0800

    初学爬虫

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var superAgent  =require('superagent');
+var cheerio=require('cheerio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  superAgent.get('http://99114.com')
+      .end(function(err,sres){
+    if(err){
+      return next(err);
+    }
+        var $ = cheerio.load(sres.text);
+        var items=[];
+        $('a[rel="nofollow"]').each(function (idx, element) {
+          var $element = $(element);
+          items.push({
+            title: $element.text(),
+
+          });
+        });
+
+        res.send(items);
+  });
+
 });
 
 module.exports = router;
