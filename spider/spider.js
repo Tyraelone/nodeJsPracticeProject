f22ed8fccf3f251808607abbfe5e026c04b7ef28
Administrator <duanziyu3@163.com>
2015-11-24 17:40:26 +0800

    支持按条件查询，简单实现分页

--- spider/spider.js
+++ spider/spider.js
@@ -6,28 +6,38 @@ var cheerio=require('cheerio');
 
 var init=function(io){
     io.on('connection',function(socket){
-        socket.on("spiderStart",function(){
-            superAgent.get('http://99114.com')
-                .end(function(err,sres){
-                    if(err){
-                        return;
-                    }
-                    var $ = cheerio.load(sres.text);
-                    var items=[];
-                    $('a[rel="nofollow"]').each(function (idx, element) {
-                        var $element = $(element);
-                        items.push({
-                            title: $element.text()
-
-                        });
-                        io.emit("data",$element.text());
-
-                    });
-
-
-                });
+        socket.on("spiderStart",function(data){
+            getShopData(io,encodeURI('http://gongying.99114.com/listing/'+data.name));
         });
     });
+}
+
+function getShopData(io,url){
+    superAgent.get(url)
+        .end(function(err,sres){
+            if(err){
+                return;
+            }
+            var $ = cheerio.load(sres.text);
+            var items=[];
+            $('div.item').each(function (idx, element) {
+                var $element = $(element);
+               var price = $element.find("div.price").text();
+                var name=$element.find(".J_U2IStat").text();
+                io.emit("data",{name:name,price:price});
+
+            });
+           var newurl=$(".m-page li.next a").attr("href");
+            if(newurl){
+
+
+                newurl="http://gongying.99114.com"+newurl;
+                newurl=encodeURI(newurl);
+                getShopData(io,newurl);
+            }
+
+
+        });
 }
 
 module.exports=init;
